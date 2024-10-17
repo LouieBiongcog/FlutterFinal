643e30f401cab62294f23ef6026e636584554985
iLoveForLoop <jefbay110@gmail.com>
2024-10-17 09:08:24 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,103 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-final myIcon = Icon(
-  Icons.settings,
-  size: 50,
-  color: Color.fromARGB(255, 17, 0, 255),
-);
-
-final star = Icon(
-  Icons.star,
-  size: 50,
-  color: const Color.fromARGB(255, 229, 255, 0),
-);
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Roboto',
-      ),
-      home: const MyHomePage(),
+      title: 'Jeferson_Bayking_MidtermExam',
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Item> items = [
+    Item('Java'),
+    Item('C#'),
+    Item('Rust'),
+    Item('Go'),
+    Item('PHP'),
+    Item('JavaScript'),
+    Item('Ruby'),
+    Item('C++'),
+    Item('Bisaya'),
+    Item('Tagalog'),
+    Item('Spanish'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage('assets/hakari.jpeg'),
-                  ),
-                  SizedBox(width: 36),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Kinji Hakari',
-                          style: TextStyle(fontSize: 22),
-                        ),
-                        Text(
-                          'Jujutsu Kaisen',
-                          style: TextStyle(fontSize: 14),
-                        ),
-                        SizedBox(height: 10),
-                        SizedBox(
-                          width: double.infinity, 
-                          child: ElevatedButton.icon(
-                            onPressed: () {
-                              print('Confirmation Successful!');
-                            },
-                            icon: Icon(Icons.check),
-                            label: Text('Confirm'),
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        SizedBox(
-                          width: double.infinity, 
-                          child: ElevatedButton.icon(
-                            onPressed: () {
-                              print('Confirmation Successful!');
-                            },
-                            icon: Icon(Icons.check),
-                            label: Text('Confirm'),
-                          ),
-                        ),
-                        Text('Si Raymart kay gwapo kaayo. The quick little brown fox jumps over the lazy dog near the bank of the river',
-                        style: TextStyle(
-                          fontSize: 10,
-                        ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-             
-            ],
-          ),
+      appBar: AppBar(
+        title: Text('Jeferson_Bayking_MidtermExam'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ItemCard(item: items[index], onDetails: showDetails, onAdd: markAsAdded);
+        },
+      ),
+    );
+  }
+
+  void showDetails(String itemName) {
+    print('$itemName details displayed.');
+  }
+
+  void markAsAdded(Item item) {
+    setState(() {
+      item.isAdded = true;
+    });
+  }
+}
+
+class Item {
+  String name;
+  bool isAdded = false;
+
+  Item(this.name);
+}
+
+class ItemCard extends StatelessWidget {
+  final Item item;
+  final Function(String) onDetails;
+  final Function(Item) onAdd;
+
+  ItemCard({required this.item, required this.onDetails, required this.onAdd});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: item.isAdded ? Colors.green : const Color.fromARGB(255, 172, 155, 155),
+      margin: EdgeInsets.all(10.0),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(item.name, style: TextStyle(fontSize: 18)),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => onDetails(item.name),
+                  child: Text('Details'),
+                ),
+                ElevatedButton(
+                  onPressed: () => onAdd(item),
+                  child: Text(item.isAdded ? 'Added' : 'Add'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
